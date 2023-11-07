b055b0d00e7f3cb790bd3f92b8a74757e0c536f5
Aidan Melvin <aidmelvin.314@gmail.com>
2023-11-07 11:54:22 -0500

    uploading final solutions

--- src/components/RadioButtonGroup.js
+++ src/components/RadioButtonGroup.js
@@ -12,11 +12,25 @@ const RadioButtonGroup = () => {
   const handleRadioButtonClick = (event) => {
     /* TODO: use the hook from line 10 to change the selectedNumber */
     /* hint: you can get the new number with event.target.value */
+    setSelectedNumber(Number(event.target.value))
   };
 
   const getColor = () => {
     switch (selectedNumber) {
       /* TODO: write logic to return the appropriate color */
+        case 1:
+          return 'red';
+        case 2:
+          return 'blue';
+        case 3:
+          return 'green';
+        case 4:
+          return 'aqua';
+        case 5:
+          return 'purple';
+        default:
+          return 'black';
+    
     }
   };
   
